5cceb3791d2b77e74049674126242b50ecbecb43
tao <tao.spa2003@gmail.com>
2024-04-23 22:57:05 +0200

    started module 0 : ex00

---
+++ module-00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <cctype>
+#include <string>
+#include <iostream>
+
+int	main(int argc, char *argv[]) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	} else {
+		for (int i = 1 ; i < argc ; i++) {
+			std::string	string(argv[i]);
+			for (size_t j = 0 ; j < string.size() ; j++) {
+				std::cout << (char) toupper(string[j]);
+			}
+		}
+	}
+	return (0);
+}
