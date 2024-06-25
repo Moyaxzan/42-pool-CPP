1c8a30e0c422795f49ce9d3d6b14a9083ca8e029
Moyaxzan <tao.spa2003@gmail.com>
2024-06-25 21:03:56 +0200

    started ex02 (module-00)

---
+++ module-00/ex02/Account.cpp
@@ -0,0 +1,22 @@
+#include "Account.hpp"
+
+int	_nbAccounts = 0;
+
+int	Account::getNbAccounts(void) {
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void ) {
+
+}
+int	Account::getNbDeposits( void ) {
+
+}
+
+int	Account::getNbWithdrawals( void ) {
+
+}
+
+void	Account::displayAccountsInfos( void ) {
+
+}
