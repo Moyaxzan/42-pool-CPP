60f977744eb35602e813052803908355d0c5e71f
tao <tao.spa2003@gmail.com>
2024-08-16 15:53:07 +0200

    module03: ex02 -> started FragTrap (should be done) except tests in main function

---
+++ module-03/ex02/ClapTrap.cpp
@@ -0,0 +1,90 @@
+#include "ClapTrap.hpp"
+#include <iostream>
+#include <string>
+
+ClapTrap::ClapTrap(std::string name) {
+	std::cout << "ClapTrap constructor called" << std::endl;
+	this->name = name;
+	this->hitPoints = 10;
+	this->energyPoints = 10;
+	this->attackDamage = 0;
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << "ClapTrap destructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other) {
+	std::cout << "ClapTrap constructor called" << std::endl;
+	this->name = other.name;
+	this->hitPoints = other.hitPoints;
+	this->energyPoints = other.energyPoints;
+	this->attackDamage = other.attackDamage;
+}
+
+ClapTrap	&ClapTrap::operator=(const ClapTrap &other) {
+	std::cout << "ClapTrap assignment operator called" << std::endl;
+	this->name = other.name;
+	this->hitPoints = other.hitPoints;
+	this->energyPoints = other.energyPoints;
+	this->attackDamage = other.attackDamage;
+	return (*this);
+}
+
+std::string		ClapTrap::getName(void) {
+	return (this->name);
+}
+
+unsigned int	ClapTrap::getHitPoints(void) {
+	return (this->hitPoints);
+}
+
+unsigned int	ClapTrap::getEnergyPoints(void) {
+	return (this->energyPoints);
+}
+
+unsigned int	ClapTrap::getAttackDamages(void) {
+	return (this->attackDamage);
+}
+
+void	ClapTrap::attack(const std::string& target) {
+	if (!this->hitPoints) {
+		std::cout << "ClapTrap " << this->name << " has no more hit points" << std::endl;
+		return ;
+	}
+	if (!this->energyPoints) {
+		std::cout << "ClapTrap " << this->name << " has no more energy points" << std::endl;
+		return ;
+	}
+	this->energyPoints -= 1;
+	std::cout << "ClapTrap " << this->name << " attacks " << target;
+	std::cout << ", causing " << this->attackDamage << " points of damage!" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount) {
+	if (amount >= this->hitPoints) {
+		this->hitPoints = 0;
+	} else {
+		this->hitPoints -= amount;
+	}
+	std::cout << "ClapTrap " << this->name << " took " << amount << " damages!" << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount) {
+	if (!this->hitPoints) {
+		std::cout << "ClapTrap " << this->name << " has no more hit points" << std::endl;
+		return ;
+	}
+	if (!this->energyPoints) {
+		std::cout << "ClapTrap " << this->name << " has no more energy points" << std::endl;
+		return ;
+	}
+	if (amount + this->hitPoints >= 10) {
+		this->hitPoints = 10;
+	} else {
+		this->hitPoints += amount;
+	}
+	this->energyPoints -= 1;
+	std::cout << "ClapTrap " << this->name << " has been repared by ";
+	std::cout << amount << " hit points!" << std::endl;
+}
---
+++ module-03/ex02/FragTrap.cpp
@@ -0,0 +1,48 @@
+#include "FragTrap.hpp"
+#include "ClapTrap.hpp"
+#include <iostream>
+#include <string>
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name) {
+	this->hitPoints = 100;
+	this->energyPoints = 100;
+	this->attackDamage = 30;
+	std::cout << "Hi, I'm " << name << " FragTrap, reporting for duty!" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap &other) : ClapTrap(other.name) { 
+	this->attackDamage = other.attackDamage;
+	this->energyPoints = other.energyPoints;
+	this->hitPoints = other.hitPoints;
+	this->name = other.name;
+}
+
+FragTrap	&FragTrap::operator=(const FragTrap &other) {
+	this->attackDamage = other.attackDamage;
+	this->energyPoints = other.energyPoints;
+	this->hitPoints = other.hitPoints;
+	this->name = other.name;
+	return (*this);
+}
+
+FragTrap::~FragTrap() {
+	std::cout << "I hope " << name << " has been usefull enough, bye..." << std::endl;
+}
+
+void	FragTrap::attack(std::string target) {
+	if (!this->hitPoints) {
+		std::cout << "FragTrap " << this->name << " has no more hit points" << std::endl;
+		return ;
+	}
+	if (!this->energyPoints) {
+		std::cout << "FragTrap " << this->name << " has no more energy points" << std::endl;
+		return ;
+	}
+	this->energyPoints -= 1;
+	std::cout << "FragTrap  " << this->name << ": attacking " << target << "!";
+	std::cout << " I dealt " << this->attackDamage << " points of damages!" << std::endl;
+}
+
+void	FragTrap::highFivesGuys(void) {
+	std::cout << "FragTrap " << this->name << ": give me a high five !" << std::endl;
+}
