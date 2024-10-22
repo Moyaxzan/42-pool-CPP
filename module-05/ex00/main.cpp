3e42806f79a7440ba1d4d1b95251c1e9fa113b32
tao <tao.spa2003@gmail.com>
2024-10-22 12:07:09 +0200

    module05: ex00: enhanced main.cpp

--- module-05/ex00/main.cpp
+++ module-05/ex00/main.cpp
@@ -11,11 +11,24 @@ int main(void) {
 			std::cout << e.what() << std::endl;
 		}
 		std::cout << bigboss << std::endl;
+		try {
+			bigboss.decrementGrade();
+		} catch (std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << bigboss << std::endl;
 	}
 	{
 		Bureaucrat larbin("stagiaire", 150);
 		try {
 			larbin.decrementGrade();
+		} catch (std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << larbin << std::endl;
+
+		try {
+			larbin.incrementGrade();
 		} catch (std::exception& e) {
 			std::cout << e.what() << std::endl;
 		}
