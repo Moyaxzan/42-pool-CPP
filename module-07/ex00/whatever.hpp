b35dc2194c1424f776431b947b8c5e51769b9184
Moyaxzan <tao.spa2003@gmail.com>
2024-11-06 13:10:22 +0100

    module-06: ex00: started

---
+++ module-07/ex00/whatever.hpp
@@ -0,0 +1,9 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T> void swap(T& p1, T& p2);
+template <typename T> T min(T& a, T& b);
+template <typename T> T max(T& a, T& b);
+
+
+#endif // !WHATEVER_HPP
