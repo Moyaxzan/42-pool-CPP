bf134f9542e97c0f948fa3059cff8e54d41945f7
Moyaxzan <tao.spa2003@gmail.com>
2024-11-08 14:32:20 +0100

    module06: ex00: main cleanup

--- module-06/ex00/main.cpp
+++ module-06/ex00/main.cpp
@@ -7,14 +7,4 @@ int main(int argc, char *argv[]) {
 		converter.convert(argv[1]);
 	}
 	return (0);
-
-	// std::string str = "350282300000000000000000000000000000000,10";
-    // Converting string to float 
-    // float str_float = static_cast<float>(atof(str.c_str())); 
-  
-    // Converting string to double 
- //    double str_double = static_cast<double>(atof(str.c_str())); 
- //  
-	// std::cout<< "string as float = " << str_float << std::endl; 
-	// std::cout<< "string as double = " << str_double << std::endl;
 }
