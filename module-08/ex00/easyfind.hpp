9b88637a168803bd4c8261b0abf695d4a2860b15
Moyaxzan <tao.spa2003@gmail.com>
2024-11-20 15:40:36 +0100

    module08: ex00: cleared eadyfind

--- module-08/ex00/easyfind.hpp
+++ module-08/ex00/easyfind.hpp
@@ -5,10 +5,9 @@
 
 template<typename T>
 int easyfind(T container, int to_find) {
-	typename T::iterator found;
-	found = std::find(container.begin(), container.end(), to_find);
-	if (found != container.end()) {
-		return (*found);
+	typename T::iterator it = std::find(container.begin(), container.end(), to_find);
+	if (it != container.end()) {
+		return (*it);
 	}
 	throw std::runtime_error("value not found in container");
 }
